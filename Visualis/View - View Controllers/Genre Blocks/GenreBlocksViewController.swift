5111abcbf6f21c30178f8e1eaed586763dcf16dd
Lance Russ <lance.r.russ@gmail.com>
2017-11-22 10:04:10 -0600

    add view controller files

---
+++ Visualis/View - View Controllers/Genre Blocks/GenreBlocksViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GenreBlocksViewController.swift
+//  Visualis
+//
+//  Created by Lance Russ on 11/22/17.
+//  Copyright © 2017 Lance Russ. All rights reserved.
+//
+
+import UIKit
+
+class GenreBlocksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Visualis/View - View Controllers/Select Filter/SelectFilterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SelectFilterViewController.swift
+//  Visualis
+//
+//  Created by Lance Russ on 11/22/17.
+//  Copyright © 2017 Lance Russ. All rights reserved.
+//
+
+import UIKit
+
+class SelectFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
