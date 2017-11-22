4d391c86f4d2a5c5095c059ae08fef7297548fd1
Lance Russ <lance.r.russ@gmail.com>
2017-11-22 10:09:16 -0600

    add network classes

---
+++ Visualis/Controller/LastfmAPIHandler.swift
@@ -0,0 +1,9 @@
+//
+//  LastfmAPIHandler.swift
+//  Visualis
+//
+//  Created by Lance Russ on 11/22/17.
+//  Copyright © 2017 Lance Russ. All rights reserved.
+//
+
+import Foundation
---
+++ Visualis/Controller/NetworkLayer.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkLayer.swift
+//  Visualis
+//
+//  Created by Lance Russ on 11/22/17.
+//  Copyright © 2017 Lance Russ. All rights reserved.
+//
+
+import Foundation
